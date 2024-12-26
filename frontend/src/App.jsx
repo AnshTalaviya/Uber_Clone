72267a9ef19a254f41cc3f3082c765f561d32b04
Ansh Talaviya <anshtalaviya22@gmail.com>
2024-12-26 17:36:06 +0530

    code push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Start from './pages/Start'
+import UserLogin from './pages/UserLogin'
 import 'remixicon/fonts/remixicon.css'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <div>
       <Routes>
         <Route path='/' element={<Start />} />
+        <Route path='/login' element={<UserLogin />} />
       </Routes>
     </div>
   )
