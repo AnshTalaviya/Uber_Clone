fbb14a63c6b8e7640b94322d3b15916bf972c064
Ansh Talaviya <anshtalaviya22@gmail.com>
2024-12-26 16:55:45 +0530

    code push

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 5050
+const { initializeSocket } = require('./socket');
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-
+initializeSocket(server);
 
 server.listen(port, () => {
-    console.log('Server listening on port ' + port)
-});    
+    console.log(`Server is running on port ${port}`);
+});
